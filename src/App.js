918909d4d50ff24b2f0ac099fc572586ee88dbbe
Yan Tsishko <yantsishko@gmail.com>
2019-06-23 00:14:55 +0300

    Documents list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CreateDocument from './document/create';
+import DocumentsList from './document/DocumentsList';
 import * as fluence from "fluence";
 
 import './App.css';
 
-import { getUserAgreements } from './helpers/agreements';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  if (!localStorage.getItem('user')) {
-    localStorage.setItem('user', '1');
-  }
+    this.state = {
+      isloadedFluence: false,
+    };
 
-  let privateKey = "569ae4fed4b0485848d3cf9bbe3723f5783aadd0d5f6fd83e18b45ac22496859"; // Authorization private key
-  let contract = "0xeFF91455de6D4CF57C141bD8bF819E5f873c1A01";                         // Fluence contract address
-  let appId = 258;                                                                      // Deployed database id
-  let ethereumUrl = "http://geth.fluence.one:8545";                                    // Ethereum light node URL
+    if (!localStorage.getItem('user')) {
+      localStorage.setItem('user', '1');
+    }
 
-  fluence.connect(contract, appId, ethereumUrl, privateKey).then((s) => {
-    console.log("Session created");
-    window.session = s;
-  });
+    let privateKey = "569ae4fed4b0485848d3cf9bbe3723f5783aadd0d5f6fd83e18b45ac22496859"; // Authorization private key
+    let contract = "0xeFF91455de6D4CF57C141bD8bF819E5f873c1A01";                         // Fluence contract address
+    let appId = 258;                                                                      // Deployed database id
+    let ethereumUrl = "http://geth.fluence.one:8545";                                    // Ethereum light node URL
 
-  window.arweave.network.getInfo().then(console.log);
+    fluence.connect(contract, appId, ethereumUrl, privateKey).then((s) => {
+      console.log("Session created");
+      window.session = s;
+      this.setState({
+        isloadedFluence: true,
+      });
+    });
 
-  const test = async () => {
-    console.log(await getUserAgreements(+localStorage.getItem('user')));
+    window.arweave.network.getInfo().then(console.log);
   }
 
   // CREATE TABLE users(id int, name varchar(128))
@@ -34,14 +40,16 @@ function App() {
 
   // SELECT * FROM agreements LEFT JOIN users ON (users.id = argeements.userId) get agreements list
 
-  return (
-    <div className="container">
-      <CreateDocument />
-      <button
-        onClick={test}
-      >Test</button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <CreateDocument />
+        {
+          this.state.isloadedFluence &&  <DocumentsList />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/document/DocumentsList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { getUserAgreements } from './../helpers/agreements';
+
+
+export default class DocumentsList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      agreements: [],
+    }
+  }
+
+  async componentDidMount() {
+    const agreements = await getUserAgreements(+localStorage.getItem('user'));
+
+    this.setState({
+      agreements,
+    });
+  }
+
+  renderItem = (item) => {
+    if (item.pathtopdf === '') {
+      return (
+        <div>
+          {item.id} - {item.participantname} - Waiting signature
+        </div>
+      )
+    }
+
+    if (item.pathtopdf !== '') {
+      return (
+        <div>
+          {item.id} - {item.participantname} - {`http://arweave.net/${item.pathtopdf}`}
+        </div>
+      )
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Created documents</h2>
+        {
+          this.state.agreements.map((item) => (
+            <div key={item.id}>
+              {this.renderItem(item)}
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+
+}
